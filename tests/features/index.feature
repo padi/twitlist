5f9f27fc44023a7d1afbb9f4e1ee458b550f598b
Padi Ignacio <marcrendlignacio@gmail.com>
2012-02-14 13:19:05 +0800

    Replaced selenium with splinter for browser automation
    Adjusted indentation

--- tests/features/index.feature
+++ tests/features/index.feature
@@ -1,5 +1,5 @@
 Feature: Index Page
 
-    Scenario: Checking the index page
-        Given I access the url "/"
-        Then I see the header "TwitList"
+  Scenario: Checking the index page
+    Given I access the url "/"
+    Then I see the header "TwitList"
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -1,23 +1,20 @@
 from lettuce import *
-from selenium import webdriver
+from splinter.browser import Browser
 from nose.tools import assert_equals
 
 @before.all
 def set_browser():
-	world.browser = webdriver.Firefox()
-	print "SET BROWSER"
+  world.browser = Browser()
 
 @after.all
 def kill_browser(total):
-	world.browser.close()
+  world.browser.quit()
 
 @step(u'Given I access the url "([^"]*)"')
 def access_url(step, directory):
-	url = "localhost:8080" + directory
-	world.browser.get(url)
+  url = "localhost:8080" + directory
+  world.browser.visit(url)
 
 @step(u'Then I see the header "([^"]*)"')
 def see_header(step, text):
-	assert False, 'This step must be implemented'
-	# header = world.dom.cssselect('h1')[0]
-	# assert header.text == text
+  assert world.browser.is_text_present('TwitList'), "header 'Twitlist' not found"
